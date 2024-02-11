336c450c2d217cbcef645e7513c7523c4c83dd24
NikolayIgnat <1126ignatenkonikolay@gmail.com>
2024-02-11 13:41:54 +0300

    Task 2 pass

--- ConsoleApp1/htask1.cs
+++ ConsoleApp1/htask1.cs
@@ -14,6 +14,14 @@ namespace homeWork1
             WriteLine($"Before: {a}     After: {ZeroingTens(a)}");
             a = -678;
             WriteLine($"Before: {a}     After: {ZeroingTens(a)}");
+
+            print_problem(2);
+            int x = 1; int y = 1;
+            WriteLine($"Клетка ({x}, {y}) -> {GetPointColor(x, y)}");
+            x = 1; y = 2;
+            WriteLine($"Клетка ({x}, {y}) -> {GetPointColor(x, y)}");
+            x = 3; y = 4;
+            WriteLine($"Клетка ({x}, {y}) -> {GetPointColor(x, y)}");
         }
 
 
@@ -24,8 +32,9 @@ namespace homeWork1
             WriteLine(); 
         }
 
-        // in  -> трёхзначное число
-        // out -> это же число с 0 вместо разряда десятков
+        // task 1
+        // in  -> int a трёхзначное число
+        // out -> int это же число с 0 вместо разряда десятков
         static int ZeroingTens(int a) 
         {
             int sgn = Sign(a);
@@ -35,5 +44,24 @@ namespace homeWork1
 
             return (a - (a % 100 / 10) * 10) * sgn;
         }
+
+        static void Validate(int x) 
+        {
+            if ((1 > x) || (x > 8))
+                throw new ArgumentException($"{x} - не чсило от 1 до 8");
+        }
+        // task 2
+        // in  -> int x, y - координаты клетки от 1 до 8
+        // out -> string "black" или "white" - цвет клетки, если (1, 1) black
+        static string GetPointColor(int x, int y) 
+        {
+            Validate(x); Validate(y);
+
+            return ((x + y) % 2 == 0) ? "чёрная" : "белая";
+        }
+
+
+
+
     }    
 }
